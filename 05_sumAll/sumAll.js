62b9c75517ad0b78864a344fdddcc07e4b31d1c3
Souveek <souveekpal@gmail.com>
2024-10-27 15:24:50 +0000

    Added the sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,53 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let startNum = 0;
+    let endNum = 0;
+    let numList = [];
+    let finalSum = 0;
+    
+        
+        if (num1 >= 0 && num2 > 0 && Number.isInteger(num1) == true && Number.isInteger(num2) == true){
+            
+            // calculates 
+            if (num2 > num1){
+                startNum = num1;
+                endNum = num2;
+                    
+                for (let i = startNum; i <= endNum; i++){
+                numList.push(i);
+                };
+                
+                numList.forEach(sumNum);
+                
+                function sumNum(item){
+                finalSum = finalSum + item;
+                };
+            }
+            
+            if (num1 >= num2){
+                startNum = num2;
+                endNum = num1;
+                    
+                for (let i = startNum; i <= endNum; i++){
+                numList.push(i);
+                }
+            
+                numList.forEach(sumNum);
+                
+                function sumNum(item){
+                finalSum = finalSum + item;
+                };
+            }
+                
+        
+        }
+        
+        else {
+                finalSum = 'ERROR';
+        }
+        
+    //console.log(numList);	
+    return finalSum;
+
 
 };
 
