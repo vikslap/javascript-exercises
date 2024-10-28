d06ed8045a6f14ae894174c6352956f5f7121bd1
Souveek <souveekpal@gmail.com>
2024-10-28 02:51:10 +0000

    Committed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -5,9 +5,10 @@ const sumAll = function(num1, num2) {
     let finalSum = 0;
     
         
+        // Tests whether the input is greater than zero and integer
         if (num1 >= 0 && num2 > 0 && Number.isInteger(num1) == true && Number.isInteger(num2) == true){
             
-            // calculates 
+            // creates an array of numbers from staring number to ending number when ending number is bigger
             if (num2 > num1){
                 startNum = num1;
                 endNum = num2;
@@ -17,12 +18,9 @@ const sumAll = function(num1, num2) {
                 };
                 
                 numList.forEach(sumNum);
-                
-                function sumNum(item){
-                finalSum = finalSum + item;
-                };
             }
             
+            // creates an array of numbers from staring number to ending number when the starting number is bigger
             if (num1 >= num2){
                 startNum = num2;
                 endNum = num1;
@@ -32,15 +30,15 @@ const sumAll = function(num1, num2) {
                 }
             
                 numList.forEach(sumNum);
-                
-                function sumNum(item){
+            }
+
+            // calculates the sum of elements in the numList array
+            function sumNum(item){
                 finalSum = finalSum + item;
                 };
-            }
-                
         
         }
-        
+        // when the entered numbers fail the initial test
         else {
                 finalSum = 'ERROR';
         }
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  result1 = (temp - 32)/1.8;
+	return Math.round((result1 + Number.EPSILON)*10)/10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  result2 = (1.8 * temp) + 32;
+	return Math.round((result2 + Number.EPSILON)*10)/10;
+  
 };
 
 // Do not edit below this line
